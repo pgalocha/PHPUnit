55a5831f9e0de3ee85b04027f313b37b3055da25
Pedro Galocha <pgalocha93@hotmail.com>
2020-04-06 01:46:24 +0100

    DEV#Class48 - Testing Static Methods. (#7)
    Co-authored-by: pedro.galocha@cleveradvertising.com <pedro.galocha@cleveradvertising.com>

--- src/models/Mailer.php
+++ src/models/Mailer.php
@@ -6,7 +6,6 @@ use Exception;
 
 class Mailer
 {
-
     public function sendMessage($email, $message)
     {
         if(empty($email))
@@ -14,6 +13,30 @@ class Mailer
             throw new Exception('Email is empty');
         }
         sleep(3);
+        echo 'send '.$message . ' to '. $email;
+
+        return true;
+    }
+
+    public function send($email, $message)
+    {
+        if(empty($email))
+        {
+            throw new Exception('Email is empty');
+        }
+
+        echo 'send '.$message . ' to '. $email;
+
+        return true;
+    }
+
+    public static function sendStatic($email, $message)
+    {
+        if(empty($email))
+        {
+            throw new Exception('Email is empty');
+        }
+
         echo 'send '.$message . ' to '. $email;
 
         return true;
